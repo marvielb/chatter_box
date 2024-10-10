f61aaa59c3af2322e0c7bbc7703811809f900abc
Marviel Billante <marvielb@gmail.com>
2024-10-10 20:36:00 +0800

    Mobile view layout

--- lib/chatterbox_web/live/room_live.ex
+++ lib/chatterbox_web/live/room_live.ex
@@ -4,49 +4,56 @@ defmodule ChatterboxWeb.RoomLive do
 
   def render(assigns) do
     ~H"""
-    <div class="w-full text-center" id="queue-container" phx-hook="Queue">
-      This is one of the rooms of all time: <%= @room_id %>
-    </div>
-    <div class="max-w-sm">
-      <ul class="flex flex-col-reverse gap-1">
-        <%= for message <- @messages do %>
-          <li class={if message.sender_id == @user_id, do: ["self-end"]}>
-            <div class={
-              if(message.sender_id == @user_id,
-                do: ["bg-amber-950 text-white rounded-bl-2xl"],
-                else: ["bg-zinc-300 text-black rounded-br-2xl"]
-              ) ++ ["rounded-t-2xl w-fit px-3 py-2"]
-            }>
-              <%= message.content %>
-            </div>
-          </li>
-        <% end %>
-      </ul>
-      <.form class="mt-5" phx-change="validate" for={@form} phx-submit="save" id="chat-form">
-        <div class="flex justify-between w-full bg-zinc-100 max-h-12 py-3 px-4 rounded-md gap-3">
-          <input
-            class="text-sm p-1 w-full bg-transparent border-transparent focus:border-transparent focus:ring-0 placeholder-stone-600 text-stone-900"
-            type="text"
-            name={@form[:message].name}
-            value={@form[:message].value}
-            placeholder="Type a message"
-          />
-          <button>
-            <svg
-              width="19"
-              height="16"
-              viewBox="0 0 19 16"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M0.674349 15.5L18.166 8L0.674349 0.5L0.666016 6.33333L13.166 8L0.666016 9.66667L0.674349 15.5Z"
-                fill="#D0D0D0"
-              />
-            </svg>
-          </button>
-        </div>
-      </.form>
+    <div class="flex flex-col gap-2 h-full">
+      <div class="flex flex-col gap-1">
+        <div class="bg-amber-800 w-full aspect-video rounded-lg">&nbsp</div>
+        <div class="bg-amber-800 w-full aspect-video rounded-lg">&nbsp</div>
+      </div>
+      <div
+        class="flex-grow flex flex-col justify-end max-h-full overflow-auto"
+        id="chat-container"
+        phx-hook="Queue"
+      >
+        <ul class="flex flex-col-reverse gap-1 overflow-y-auto">
+          <%= for message <- @messages do %>
+            <li class={if message.sender_id == @user_id, do: ["self-end"]}>
+              <div class={
+                if(message.sender_id == @user_id,
+                  do: ["bg-amber-950 text-white rounded-bl-2xl"],
+                  else: ["bg-zinc-300 text-black rounded-br-2xl"]
+                ) ++ ["rounded-t-2xl w-fit px-3 py-2"]
+              }>
+                <%= message.content %>
+              </div>
+            </li>
+          <% end %>
+        </ul>
+        <.form class="mt-5" phx-change="validate" for={@form} phx-submit="save" id="chat-form">
+          <div class="flex justify-between w-full bg-zinc-100 max-h-12 py-3 px-4 rounded-md gap-3">
+            <input
+              class="text-sm p-1 w-full bg-transparent border-transparent focus:border-transparent focus:ring-0 placeholder-stone-600 text-stone-900"
+              type="text"
+              name={@form[:message].name}
+              value={@form[:message].value}
+              placeholder="Type a message"
+            />
+            <button>
+              <svg
+                width="19"
+                height="16"
+                viewBox="0 0 19 16"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M0.674349 15.5L18.166 8L0.674349 0.5L0.666016 6.33333L13.166 8L0.666016 9.66667L0.674349 15.5Z"
+                  fill="#D0D0D0"
+                />
+              </svg>
+            </button>
+          </div>
+        </.form>
+      </div>
     </div>
     """
   end
